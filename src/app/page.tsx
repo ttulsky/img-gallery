90f3787dcee3228fcf8c1028d725fa1ee26aa5fd
Tyler Tulsky <ttulsky@gmail.com>
2023-07-30 11:19:48 +0900

    homepage update text and links

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,19 @@
 export default function Home() {
-  return <div>Home Page</div>;
+  return (
+    <div className="home_text">
+      <h1>Welcome to the NextJS Image Genorator!</h1>
+      <h2></h2>
+      <p>
+        Images brought to you by the{" "}
+        <a href="https://unsplash.com/">Upspalsh.com </a>API
+      </p>
+      <p>
+        Powered by Vercels
+        <a href="https://vercel.com/">
+          <span style={{ color: "mediumorchid" }}>NextJS </span>
+        </a>
+        13.4.12
+      </p>
+    </div>
+  );
 }
