d28ac646fdbbd8215af4ea28aef4b1177d96f504
Tyler Tulsky <ttulsky@gmail.com>
2023-07-31 18:00:22 +0900

    update for eslint

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,17 @@
 export default function Home() {
   return (
     <div>
-      <h1>Welcome to the NextJS Image Genorator!</h1>
+      <h1>Welcome to the NextJS Image Generator!</h1>
       <h2></h2>
       <p>
         Images brought to you by the{" "}
         <a href="https://unsplash.com/" style={{ textDecoration: "none" }}>
-          Unspalsh.com
+          Unsplash.com
         </a>{" "}
         API
       </p>
       <p>
-        Powered by Vercel's
+        Powered by Vercel&apos;s
         <a href="https://vercel.com/" style={{ textDecoration: "none" }}>
           <span style={{ color: "mediumorchid" }}> NextJS </span>
         </a>
