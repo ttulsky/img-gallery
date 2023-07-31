111c96110d3634f23b84eb264edd4c517a2133b9
Tyler Tulsky <ttulsky@gmail.com>
2023-07-31 17:26:57 +0900

    style adjust

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,18 @@
 export default function Home() {
   return (
-    <div className="home_text">
+    <div>
       <h1>Welcome to the NextJS Image Genorator!</h1>
       <h2></h2>
       <p>
         Images brought to you by the{" "}
-        <a href="https://unsplash.com/"> Unspalsh.com </a>API
+        <a href="https://unsplash.com/" style={{ textDecoration: "none" }}>
+          Unspalsh.com
+        </a>{" "}
+        API
       </p>
       <p>
-        Powered by Vercels
-        <a href="https://vercel.com/">
+        Powered by Vercel's
+        <a href="https://vercel.com/" style={{ textDecoration: "none" }}>
           <span style={{ color: "mediumorchid" }}> NextJS </span>
         </a>
         13.4.12
